9da8fa4af6e7b2edc2b84bdea119f9f8c96b4a30
kushal <kvmhjn@gmail.com>
2021-11-21 18:30:33 +0530

    Updates

---
+++ precache-manifest.0d553fe73da6a0004154ea4bf53135d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8296e6a269b7b5849434a16a1959a531",
+    "url": "./index.html"
+  },
+  {
+    "revision": "006cc6b3b4a5a33054bc",
+    "url": "./static/css/main.1669231a.chunk.css"
+  },
+  {
+    "revision": "843ab5bc4618698f1524",
+    "url": "./static/js/2.ec967294.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.ec967294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006cc6b3b4a5a33054bc",
+    "url": "./static/js/main.764f3bf4.chunk.js"
+  },
+  {
+    "revision": "b45ce563ccb91bc6d4e1",
+    "url": "./static/js/runtime-main.280460f2.js"
+  }
+]);
